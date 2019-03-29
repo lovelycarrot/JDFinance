69f0474b430aa984aa0ecd9fe6ae398cfe232234
lovelycarrot <little_carrot@163.com>
2019-03-29 17:08:35 +0800

    add borrow

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-slider></home-slider>
     <home-novice></home-novice>
+    <home-borrow></home-borrow>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import homeHeader from '@/public/header'
 import homeSlider from '@/pages/home/hslider'
 import homeNovice from '@/pages/home/novice'
+import homeBorrow from '@/pages/home/borrow'
 export default {
   name: 'home',
   data () {
@@ -20,7 +22,8 @@ export default {
   components: {
     homeHeader,
     homeSlider,
-    homeNovice
+    homeNovice,
+    homeBorrow
   }
 }
 </script>
---
+++ src/pages/home/borrow.vue
@@ -0,0 +1,33 @@
+<template>
+  <panel title="极速贷款" :class="$style.panel">
+    <div>
+      <router-link :to="{name:'home'}">
+        <img src="//img12.360buyimg.com/jrpmobile/jfs/t12721/42/2497849749/26333/ec584be4/5a421756N2416c88f.png?width=750&height=280" alt="极速贷款">
+      </router-link>
+    </div>
+  </panel>
+</template>
+
+<script>
+import panel from '@/core/panel'
+export default {
+  name: 'borrow',
+  components: {
+    panel
+  }
+}
+</script>
+
+<style lang="scss" module>
+@import "@/assets/styles/element.scss";
+.panel {
+  @include panel;
+  div {
+    padding-bottom: 40px;
+    img {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+</style>
